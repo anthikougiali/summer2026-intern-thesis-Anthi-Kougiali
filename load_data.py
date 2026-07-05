5626393cd74f7e5df7b20ee072e31be75b44857e
Anthi Kougiali <anthikougiali@gmail.com>
2026-07-05 20:11:44 +0000

    part 4: labels - works - might need to revisit

--- load_data.py
+++ load_data.py
@@ -14,3 +14,12 @@ returns = close.pct_change()
 returns = returns.dropna()
 
 print(returns.head())
+
+vix_level = close["^VIX"]
+
+vix_median = vix_level.median()
+
+regime = vix_level > vix_median
+
+print(vix_median)
+print(regime.head())
