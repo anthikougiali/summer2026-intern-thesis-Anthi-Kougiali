be5bca7f384970c07334ea3f7ec92745ae3027fd
Anthi Kougiali <anthikougiali@gmail.com>
2026-07-05 19:44:05 +0000

    part 2: load all etfs but keep close prices only

--- load_data.py
+++ load_data.py
@@ -1,4 +1,14 @@
 import yfinance as yf 
 import pandas as pd
+
 data = yf.download("XBI", start="2010-01-01", end="2025-01-01")
+
 print(data.head())
+
+tickers = ["XBI", "IHI", "PPH", "IHF", "XLV", "^VIX"]
+
+data = yf.download(tickers, start="2010-01-01", end="2025-01-01")
+
+close = data["Close"]
+
+print(close.head())
